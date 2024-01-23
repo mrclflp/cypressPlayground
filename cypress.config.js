e0a3eccc34007f9a6fd156f1b6fc2410ce6a4daa
Marcel Filip <marcel@agorapulse.com>
2024-01-23 08:59:41 +0100

    adding mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'mochawesome',
     baseUrl: 'https://demo.playwright.dev/todomvc',
     setupNodeEvents(on, config) {
       // implement node event listeners here
